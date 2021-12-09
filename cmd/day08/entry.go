9e4c37698db923ae1936d4b6a92d959149d6fb28
Kalle Jillheden <kalle.jillheden@gmail.com>
2021-12-09 22:15:33 +0100

    Stringer entry

--- cmd/day08/entry.go
+++ cmd/day08/entry.go
@@ -1,5 +1,7 @@
 package main
 
+import "strings"
+
 type Segment byte
 
 const (
@@ -12,9 +14,35 @@ const (
 	SegG Segment = 'g'
 )
 
+func (s Segment) String() string {
+	return string(byte(s))
+}
+
 type Digit []Segment
+
+func (d Digit) String() string {
+	bytes := make([]byte, len(d))
+	for i, b := range d {
+		bytes[i] = byte(b)
+	}
+	return string(bytes)
+}
 
 type Entry struct {
 	SignalPatterns [10]Digit
 	OutputValue    [4]Digit
+}
+
+func (e Entry) String() string {
+	var sb strings.Builder
+	for _, d := range e.SignalPatterns {
+		sb.WriteString(d.String())
+		sb.WriteByte(' ')
+	}
+	sb.WriteByte('|')
+	for _, d := range e.OutputValue {
+		sb.WriteByte(' ')
+		sb.WriteString(d.String())
+	}
+	return sb.String()
 }
