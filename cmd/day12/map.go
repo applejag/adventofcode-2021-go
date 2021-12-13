e7f222c5166a1f08d8a799a24126073173f9d37a
Kalle Jillheden <kalle.jillheden@gmail.com>
2021-12-13 20:42:27 +0100

    Day 12 part 2

--- cmd/day12/map.go
+++ cmd/day12/map.go
@@ -1,12 +1,18 @@
 package main
 
+import "github.com/jilleJr/adventofcode-2021-go/internal/common"
+
 type Map map[Cave][]Cave
 
 func (m Map) CountPaths() int {
-	return m.countPathsRec(CaveStart, Path{CaveStart})
+	if common.Part2 {
+		return m.countPathsRecPart2(CaveStart, Path{CaveStart}, false)
+	} else {
+		return m.countPathsRecPart1(CaveStart, Path{CaveStart})
+	}
 }
 
-func (m Map) countPathsRec(from Cave, pathTaken Path) int {
+func (m Map) countPathsRecPart1(from Cave, pathTaken Path) int {
 	if from == CaveEnd {
 		log.Debug().WithStringer("path", pathTaken).Message("")
 		return 1
@@ -17,7 +23,29 @@ func (m Map) countPathsRec(from Cave, pathTaken Path) int {
 			continue
 		}
 		//log.Debug().WithStringf("path", "%5s -> %-5s", from, to).Message("")
-		paths += m.countPathsRec(to, append(pathTaken, to))
+		paths += m.countPathsRecPart1(to, append(pathTaken, to))
+	}
+	return paths
+}
+
+func (m Map) countPathsRecPart2(from Cave, pathTaken Path, revisitSmall bool) int {
+	if from == CaveEnd {
+		log.Debug().WithStringer("path", pathTaken).Message("")
+		return 1
+	}
+	var paths int
+	for _, to := range m[from] {
+		if to == CaveStart {
+			continue
+		}
+		if to.IsSmall() && containsCave(pathTaken, to) {
+			if !revisitSmall {
+				paths += m.countPathsRecPart2(to, append(pathTaken, to), true)
+			}
+			continue
+		}
+		//log.Debug().WithStringf("path", "%5s -> %-5s", from, to).Message("")
+		paths += m.countPathsRecPart2(to, append(pathTaken, to), revisitSmall)
 	}
 	return paths
 }
